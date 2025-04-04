5bd4b686b1b9b789915a055a0bba10a5a8d25847
Dongmin <emthrology@gmail.com>
2025-04-04 18:53:17 +0900

    에디터 이미지 플러그인 에러 잡기

--- plugin/lexical/ImagePlugin.js
+++ plugin/lexical/ImagePlugin.js
@@ -5,29 +5,19 @@ export function ImagePlugin() {
   const [editor] = useLexicalComposerContext(); // 올바른 에디터 인스턴스 획득
   const apiUrl = process.env.NEXT_PUBLIC_API_URL;
   const handleInsertImage = (url, alt) => {
-    // const url = 'https://postfiles.pstatic.net/MjAyNTA0MDFfNzQg/MDAxNzQzNTE4NzM5NjMw.fOvQi8wrgIzcA22Dv6AZ1pv0t5iBL9tmmdH0o_Bk_rEg.w4mCIxA19Og8FuW7NYCX-F57vFdL_mj4Di9pIP_npocg.JPEG/shinkawayua_art_20180809.jpg?type=w966';
     editor.update(() => {
       const imageNode = new ImageNode(
         url,
         alt || '',
       );
-      // const root = editor.getRoot();
       const root = $getRoot();
       root.append(imageNode);
     });
   };
-  const handleFileUpload = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        handleInsertImage(reader.result); // Base64 데이터 URL 사용
-        e.target.value = ''; // 1. 입력 값 초기화
-      };
-      reader.readAsDataURL(file);
-    }
-  };
   const uploadAndInsertImage = async (e) => {
+    if (e.target.files === undefined) {
+      return;
+    }
     const file = e.target.files[0];
     if (file) {
       const formatData = new FormData();
@@ -39,7 +29,7 @@ export function ImagePlugin() {
           body: formatData,
           headers: {
             'Authorization': `Bearer ${localStorage.getItem('authToken')}`,
-            // 'ContentType': 'multipart/form-data'
+            // 'ContentType': 'multipart/form-data' // 이 헤더가 수동으로 설정되면 브라우저에서 경계값 을 설정하지 않으므로 주석 처리
           }
 
         });
