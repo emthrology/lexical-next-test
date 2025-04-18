0520cff10ecce834e6842380edf2f4c5e515a31d
Dongmin <emthrology@gmail.com>
2025-04-18 15:54:36 +0900

    내 정보 화면 css 및 레이아웃 변경

--- app/blogs/edit/page.jsx
+++ app/blogs/edit/page.jsx
@@ -1,10 +1,29 @@
+'use client';
 import RichTextEditor from '@/app/components/blogs/edit/RichTextEditor';
-import React from 'react';
-
-export default function page() {
+import React, { use, useState, useEffect } from 'react';
+export default function page({ searchParams }) {
+  const [blogData, setBlogData] = useState(null);
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+  const { blogId } = use(searchParams);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (blogId) {
+        try {
+          const response = await fetch(`${apiUrl}/blogs/${blogId}`);
+          if (response.ok) {
+            const data = await response.json();
+            setBlogData(data);
+          }
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }
+    };
+    fetchData();
+  }, []);
   return (
     <div className="content-wrap">
-      <RichTextEditor />
+      <RichTextEditor editData={blogData?.data || null} />
     </div>
   );
 }
